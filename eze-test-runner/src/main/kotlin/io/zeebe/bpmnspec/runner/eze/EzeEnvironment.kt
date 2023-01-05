6ef075ab157e6134f73c44c11afd7cfc12b3b962
Philipp Ossler <philipp.ossler@gmail.com>
2023-01-05 22:42:02 +0100

    fix: more reliable EZE test runner
    Catch possible error cases in the EZE test runner.

--- eze-test-runner/src/main/kotlin/io/zeebe/bpmnspec/runner/eze/EzeEnvironment.kt
+++ eze-test-runner/src/main/kotlin/io/zeebe/bpmnspec/runner/eze/EzeEnvironment.kt
@@ -3,17 +3,31 @@ package io.zeebe.bpmnspec.runner.eze
 import io.camunda.zeebe.client.ZeebeClient
 import org.camunda.community.eze.EngineFactory
 import org.camunda.community.eze.ZeebeEngine
+import org.slf4j.LoggerFactory
 
 class EzeEnvironment(
     val zeebeEngine: ZeebeEngine = EngineFactory.create()
 ) {
 
+    private val logger = LoggerFactory.getLogger(EzeEnvironment::class.java)
+
     lateinit var zeebeClient: ZeebeClient
 
     fun setup() {
-        zeebeEngine.start()
+        try {
+            zeebeEngine.start()
 
-        zeebeClient = zeebeEngine.createClient()
+            zeebeClient = zeebeEngine.createClient()
+
+        } catch (e: Exception) {
+            // clean up if the creation failed (e.g. free server ports)
+            zeebeEngine.stop()
+
+            logger.error("Failed to start the EZE engine", e)
+            
+            throw RuntimeException("Failed to start the EZE engine", e)
+        }
+
     }
 
     fun cleanUp() {
