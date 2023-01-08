d6c75a9d24f26a22534a4aecf4b687c3ce7a7fbb
Philipp Ossler <philipp.ossler@gmail.com>
2023-01-08 23:02:59 +0100

    refactor: remove Zeebe and EZE test runners
    The API for test runners was removed. Instead, the test runner can be configured by providing an environment.
    
    The EZE test runner was moved into the core module as the default environment. The Zeebe test runner that is based on testcontainers is not useful anymore (i.e. too slow).

--- eze-test-runner/src/main/kotlin/io/zeebe/bpmnspec/runner/eze/EzeTestContext.kt
+++
@@ -1,7 +0,0 @@
-package io.zeebe.bpmnspec.runner.eze
-
-import io.camunda.zeebe.client.ZeebeClient
-
-data class EzeTestContext(
-    val zeebeClient: ZeebeClient
-)
--- zeebe-test-runner/src/main/kotlin/io/zeebe/bpmnspec/runner/zeebe/ZeebeTestContext.kt
+++
@@ -1,7 +0,0 @@
-package io.zeebe.bpmnspec.runner.zeebe
-
-import io.camunda.zeebe.client.ZeebeClient
-
-data class ZeebeTestContext(
-        val zeebeClient: ZeebeClient
-)
