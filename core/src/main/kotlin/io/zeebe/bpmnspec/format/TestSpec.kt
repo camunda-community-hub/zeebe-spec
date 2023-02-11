3ab2205373831ae02cce9974ad1ba02d8888a578
Philipp Ossler <philipp.ossler@gmail.com>
2023-02-11 21:16:08 +0100

    refactor: add deprecated annotation

--- core/src/main/kotlin/io/zeebe/bpmnspec/format/TestSpec.kt
+++ core/src/main/kotlin/io/zeebe/bpmnspec/format/TestSpec.kt
@@ -1,6 +1,7 @@
 package io.zeebe.bpmnspec.format
 
 data class TestSpec(
+    @Deprecated("Explicit resources are not supported anymore.")
     val resources: List<String>? = emptyList(),
     val testCases: List<TestCase>
 )
